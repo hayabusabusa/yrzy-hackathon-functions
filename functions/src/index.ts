5a38574e7d7b14301e4129ef847d73faaa4f1ed8
hayabusabusa <hayabusabusaSY@gmail.com>
2020-12-29 16:00:23 +0900

    chore: remove dotenv package and use functions config

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,13 @@
-import { resolve } from 'path';
-import * as dotenv from 'dotenv';
 import * as functions from 'firebase-functions';
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-// NOTE: .env 読み込み用の設定
-dotenv.config({
-    path: resolve(__dirname, '../.env'),
-});
-
 export const helloWorld = functions.https.onRequest((request, response) => {
-    const channelSecret = process.env.CHANNEL_SECRET;
+    const channelSecret = functions.config().line.secret;
     if (channelSecret === undefined) {
-        functions.logger.info(resolve(__dirname, '../.env'));
-        functions.logger.info("key `CHANNEL_SECRET` is undefined in .env file.");
+        functions.logger.info("key `CHANNEL_SECRET` is undefined.");
     }
   
-    response.send(`Hello from Firebase!`);
+    response.send(`Hello from Firebase! SECRET=${channelSecret}`);
 });
