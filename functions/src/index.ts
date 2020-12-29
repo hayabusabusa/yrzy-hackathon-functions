21f148451472703bd5cf48727978f14fe55b1dd0
hayabusabusa <hayabusabusaSY@gmail.com>
2020-12-29 14:24:56 +0900

    chore: update gitignore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,13 @@
+import * as dotenv from 'dotenv';
 import * as functions from 'firebase-functions';
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
+// NOTE: .env 読み込み用の設定
+dotenv.config;
+
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  response.send(`Hello from Firebase! TOKEN=${process.env.LINE_CHANNEL_ACCESS_TOKEN}`);
 });
