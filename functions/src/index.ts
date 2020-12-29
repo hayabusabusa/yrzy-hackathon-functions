43790b4d59752ef891fa0160d3906c84abf20821
hayabusabusa <hayabusabusaSY@gmail.com>
2020-12-29 15:05:12 +0900

    chore: fix read dot env file config

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,7 +5,9 @@ import * as functions from 'firebase-functions';
 // https://firebase.google.com/docs/functions/typescript
 
 // NOTE: .env 読み込み用の設定
-dotenv.config();
+dotenv.config({
+    path: __dirname + '/.env',
+});
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
