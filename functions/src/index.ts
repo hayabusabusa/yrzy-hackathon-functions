45a18890b7becf8b09f70ea504c80ea670bc1db7
Arata Totsuka <bump.art71@gmail.com>
2021-01-02 00:45:17 +0900

    ジャンルによるフィルター処理と画像URL指定処理の追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -19,23 +19,30 @@ const app = express();
 app.post('/', line.middleware(config), (req, res, next) => {
     const json = JSON.stringify(req.body);
     const response: LineResponce = JSON.parse(json);
-    
-    //functions.logger.info(response.events[0].message.text);
 
     (async () => {
-        //const foodQuerySnapshot = await admin.firestore().collection('food').where('name', '==', response.events[0].action.label).get();
-        const foodQuerySnapshot = await admin.firestore().collection('food').get();
+        switch (response.events[0].message.text) {
+            case "和食":
+            case "洋食":
+            case "中華":
+                var foodQuerySnapshot = await admin.firestore().collection('food').where('genre', '==', response.events[0].message.text).get();
+                break;
+            default:
+                var foodQuerySnapshot = await admin.firestore().collection('food').get();
+                break;
+        }
+
         const foodDocument = foodQuerySnapshot.docs[Math.floor(Math.random() * foodQuerySnapshot.docs.length)];
         const foodName = foodDocument.data().name;
-        //const foodURL = foodDocument.data().url;
+        const imageURL = foodDocument.data().url;
 
         const storesQuerySnapshot = await admin.firestore().collection('stores').get();
         const storesDocument = storesQuerySnapshot.docs[Math.floor(Math.random() * storesQuerySnapshot.docs.length)];
         const storesName = storesDocument.data().name;
 
-        await client.replyMessage(response.events[0].replyToken, ReplyFlexMessage.create(storesName, foodName));
+        await client.replyMessage(response.events[0].replyToken, ReplyFlexMessage.create(storesName, foodName, imageURL));
     })().catch(next);
-    
+
     res.status(200).send()
 });
 
